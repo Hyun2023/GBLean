0609d5afb737d38fe5e88c153a25d683c38a4f30
Jaehyung Lee <jaehyung.lee@sf.snu.ac.kr>
2024-10-31 16:04:41 +0900

    add groebnerbasis structure

--- GB/Groebner.lean
+++ GB/Groebner.lean
@@ -6,8 +6,17 @@ import Mathlib.Algebra.Field.Defs
 import Mathlib.Algebra.MvPolynomial.Basic
 import Mathlib.Data.Finset.Basic
 import Mathlib.RingTheory.Ideal.Basic
+import Mathlib.RingTheory.Ideal.Basic
+import GB.Monomial
 -- import Mathlib.RingTheory.Ideal.Span
 
-structure GroebnerBasis (σ R : Type) [Finite σ] [fr: Field R]  where
+instance [CommRing R] : Coe (Set (Monomial σ)) (Set (MvPolynomial σ R)) where
+  coe := fun a => Set.image (fun m => MvPolynomial.monomial m 1) a
+
+structure GroebnerBasis (σ R : Type) [Finite σ] [fr: Field R] [MonomialOrder σ]  where
   G : Finset (MvPolynomial σ R)
   I : Ideal (MvPolynomial σ R)
+  G_spans_I : Ideal.span G = I
+  leading_monomial : MvPolynomial σ R -> Monomial σ
+  initial_spans_initial : Ideal.span (Set.image (fun m => MvPolynomial.monomial m 1) (Set.image leading_monomial (Finset.toSet G)))
+  = Ideal.span (Set.image (fun m => MvPolynomial.monomial m 1) (Set.image leading_monomial I))
