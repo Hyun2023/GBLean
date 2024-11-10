5de67b293c014e51937df7fb1057142c6449f21c
MerHS <mersshs@gmail.com>
2024-11-11 08:13:00 +0900

    step?

--- GB/Buchberger.lean
+++ GB/Buchberger.lean
@@ -33,11 +33,36 @@ def find_new_basis
       let r := red_comp_list gen (S gᵢ gₖ)
       if r = 0 then none else some r
 
+def buchberger_step
+  (gen: List (FiniteVarPoly σ R))
+  (nonzero_all: ∀ p ∈ gen, p ≠ 0)
+  (i k: ℕ)
+  : List (FiniteVarPoly σ R) :=
+  let n := List.length gen
+  if n ≤ 1 then
+    gen
+  else if i = 0 ∧ k ≥ (n - 1) then
+    gen
+  else if i + k + 2 > n then
+    buchberger_step gen nonzero_all (i - 1) 0
+  else
+    let fp := find_new_basis gen i 0
+    match fp with
+    | none => buchberger_step gen nonzero_all i (k + 1)
+    | some p =>
+      have hp: p ≠ 0 := by sorry
+      have hp_all: ∀ p' ∈ (p :: gen), p' ≠ 0 := by aesop
+      buchberger_step (p :: gen) hp_all i (k + 1)
+
 def buchberger_algorithm_list
   (gen: List (FiniteVarPoly σ R))
   (nonzero_all: ∀ p ∈ gen, p ≠ 0)
   : List (FiniteVarPoly σ R) :=
-  sorry
+  let n := List.length gen
+  if n ≤ 1 then
+    gen
+  else
+    buchberger_step gen nonzero_all (n - 2) 0
 
 def buchberger_algorithm
   [DecidableEq R]
