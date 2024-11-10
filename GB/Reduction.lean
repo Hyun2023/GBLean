b8df1f8374fcdc31e10456cf860d87a316cf4cb0
Yeonwoo-Nam <9921ska@gmail.com>
2024-11-10 21:33:06 +0900

    typo

--- GB/Reduction.lean
+++ GB/Reduction.lean
@@ -17,16 +17,16 @@ def MvPolynomial.div [DecidableEq σ] [Field R] (f g : MvPolynomial σ R) (g_non
 lemma MvPolynomial.div_correct [DecidableEq σ] [ord : MonomialOrder σ] [Field R] (f g : MvPolynomial σ R) (g_nonzero : g ≠ 0):
   let (h,r) := MvPolynomial.div f g g_nonzero;
   f = g*h+r ∧
-  (r ≠ 0 ∨ ∀m ∈ monomials r, ¬ Monomial.instDvd.dvd (leading_monomial g g_nonzero) m) := sorry
+  (r = 0 ∨ ∀m ∈ monomials r, ¬ Monomial.instDvd.dvd (leading_monomial g g_nonzero) m) := sorry
 
-def Mvpolynomial.multidiv [DecidableEq σ] [Field R] (f : MvPolynomial σ R) (F : Generators σ R m) (F_nonzero : ∀ i, F i ≠ 0) :
+def Mvpolynomial.multidiv [DecidableEq σ] [Field R] (f : MvPolynomial σ R) (F : Generators σ R s) (F_nonzero : ∀ i < s, F i ≠ 0) :
     (ℕ → MvPolynomial σ R) × (MvPolynomial σ R) :=
   sorry
 
-lemma Mvpolynomial.multidiv_correct [DecidableEq σ] [ord : MonomialOrder σ] [Field R] (f : MvPolynomial σ R) (F : Generators σ R m) (F_nonzero : ∀ i, F i ≠ 0):
+lemma Mvpolynomial.multidiv_correct [DecidableEq σ] [ord : MonomialOrder σ] [Field R] (f : MvPolynomial σ R) (F : Generators σ R s) (F_nonzero : ∀ i < s, F i ≠ 0):
     let (a,r) := Mvpolynomial.multidiv f F F_nonzero;
-    ∀ i≥m, a i = 0 /\
-    f = r + (∑ (i : {i | i<m}), (a i.1)*(F i.1)) /\
-    (r ≠ 0 ∨ ∀m ∈ monomials r, ∀ i, ¬ Monomial.instDvd.dvd (leading_monomial (F i) (F_nonzero i)) m) := sorry
+    ∀ i≥s, a i = 0 /\
+    f = r + (∑ (i : {i | i<s}), (a i.1)*(F i.1)) /\
+    (r = 0 ∨ ∀m ∈ monomials r, ∀ (ilt : i < s), ¬ Monomial.instDvd.dvd (leading_monomial (F i) (F_nonzero i ilt)) m) := sorry
 
 def red [DecidableEq σ] [Field R] (f : MvPolynomial σ R) : MvPolynomial σ R := sorry
