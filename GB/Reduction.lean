d38ee96b88cf413f9fc4dbbffc1c1c8e5ec3894a
Yeonwoo-Nam <9921ska@gmail.com>
2024-11-11 23:04:50 +0900

    part of FiniteVarPoly.multidiv_correct

--- GB/Reduction.lean
+++ GB/Reduction.lean
@@ -21,23 +21,47 @@ lemma FiniteVarPoly.div_correct [DecidableEq σ] [ord : MonomialOrder σ] [Field
 def FiniteVarPoly.multidiv_help [DecidableEq σ] [DecidableEq R] [LinearOrder σ] [LinearOrder R] [Field R] (s : FiniteVarPoly σ R) (F : List (FiniteVarPoly σ R)) (F_nonzero : F.all (fun f ↦ f≠0)): (CFinsupp (FiniteVarPoly σ R) (FiniteVarPoly σ R)) × (FiniteVarPoly σ R) :=
   match F with
   | [] => (CFinsupp.empty _ _, s)
-  | List.cons f F' => by
-    simp at F_nonzero; rcases F_nonzero with ⟨nonzero₁, nonzero₂⟩
-    have (h₁,r) := div s f nonzero₁
-    have (h₂,r) := multidiv_help r F' (by simp; exact nonzero₂)
-    exact (if p: h₁≠0 then CFinsupp_add h₂ f h₁ p else h₂, r)
+  | f :: F' =>
+    -- simp at F_nonzero; rcases F_nonzero with ⟨nonzero₁, nonzero₂⟩
+    let (h₁,r) := div s f (by simp at F_nonzero; rcases F_nonzero; assumption)
+    let (h₂,r) := multidiv_help r F' (by simp; simp at F_nonzero; rcases F_nonzero; assumption)
+    (if p: h₁≠0 then CFinsupp_add h₂ f h₁ p else h₂, r)
+
+lemma FList_nonzero [DecidableEq σ] [DecidableEq R] [LinearOrder σ] [LinearOrder R] [Field R] (F : Generators σ R) (F_nonzero : ∀ f ∈ F, f ≠ 0) : (FiniteVarPoly.toList F).all (fun f ↦ f≠0) := by
+  have mem_sort : ∀ f, f ∈ FiniteVarPoly.toList F ↔ f ∈ F := by
+    intro f; rw [FiniteVarPoly.toList, Finset.mem_sort]
+  rw [List.all_eq]
+  rcases em (∀ x ∈ FiniteVarPoly.toList F, decide (x ≠ 0) = true) with e1|e1 <;> simp [e1] <;>
+  intro x h <;> rw [mem_sort] at h <;> apply F_nonzero <;> assumption
 
 def FiniteVarPoly.multidiv [DecidableEq σ] [DecidableEq R] [LinearOrder σ] [LinearOrder R] [Field R] (s : FiniteVarPoly σ R) (F : Generators σ R) (F_nonzero : ∀ f ∈ F, f ≠ 0) :
-    (CFinsupp (FiniteVarPoly σ R) (FiniteVarPoly σ R)) × (FiniteVarPoly σ R) := by
-  have mem_sort : ∀ f, f ∈ toList F ↔ f ∈ F := by
-    intro f; rw [toList, Finset.mem_sort]
-  have FList_nonzero : (toList F).all (fun f ↦ f≠0) := by
-    rw [List.all_eq]
-    rcases em (∀ x ∈ toList F, decide (x ≠ 0) = true) with e1|e1 <;> simp [e1] <;>
-    intro x h <;> rw [mem_sort] at h <;> apply F_nonzero <;> assumption
-  exact multidiv_help s (toList F) FList_nonzero
+    (CFinsupp (FiniteVarPoly σ R) (FiniteVarPoly σ R)) × (FiniteVarPoly σ R) :=
+  multidiv_help s (toList F) (FList_nonzero F F_nonzero)
 
-lemma FiniteVarPoly.multidiv_correct [DecidableEq σ] [DecidableEq R] [LinearOrder σ] [LinearOrder R] [ord : MonomialOrder σ] [Field R] (f : FiniteVarPoly σ R) (F : Generators σ R) (F_nonzero : ∀ f ∈ F, f ≠ 0):
-    let (a,r) := FiniteVarPoly.multidiv f F F_nonzero;
-    f = r + (∑ (f ∈ F), (a f)*(f)) /\
-    (r = 0 ∨ ∀m ∈ monomials r, ∀ (inF : f ∈ F), ¬ Monomial.instDvd.dvd (leading_monomial f (F_nonzero f inF)) m) := sorry
+lemma FiniteVarPoly.multidiv_correct [DecidableEq σ] [DecidableEq R] [LinearOrder σ] [LinearOrder R] [ord : MonomialOrder σ] [Field R] (s : FiniteVarPoly σ R) (F : Generators σ R) (F_nonzero : ∀ f ∈ F, f ≠ 0):
+    let (a,r) := FiniteVarPoly.multidiv s F F_nonzero;
+    s = r + (∑ (f ∈ F), (a f)*(f)) /\
+    (r = 0 ∨ ∀m ∈ monomials r, ∀ f (inF : f ∈ F), ¬ Monomial.instDvd.dvd (leading_monomial f (F_nonzero f inF)) m) := by
+  unfold multidiv; simp
+  constructor
+  . have : ∀ l F F_nonzero, (EQ: l = toList F) -> s = (multidiv_help s l (by rw[EQ]; exact FList_nonzero F F_nonzero)).2 + (∑ (f ∈ F), ((multidiv_help s l (by rw[EQ]; exact FList_nonzero F F_nonzero)).1 f)*(f)) := by
+      clear F F_nonzero
+      intro l; induction' l with f F' IH
+      <;> intro F F_nonzero EQ
+      . unfold multidiv_help; simp
+        have : F = ∅ := by
+          apply Finset.eq_empty_of_forall_not_mem
+          intro f
+          have EQ' : ¬ ∃(f : FiniteVarPoly σ R), f ∈ toList F := by
+            rw [← EQ]
+            rintro ⟨f,h1,h2⟩
+          by_contra G; apply EQ'
+          exists f; unfold toList; rw [Finset.mem_sort]; assumption
+        rw [this, Finset.sum_eq_fold]; simp;
+        clear this EQ F_nonzero F
+        -- FiniteVarPoly is CommRing
+        sorry
+      .
+        sorry
+    apply this; rfl; assumption
+  . sorry
