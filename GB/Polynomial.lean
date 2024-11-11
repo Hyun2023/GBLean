7999d3ecdf64723b304de96e66033f5fa023044b
Yeonwoo-Nam <9921ska@gmail.com>
2024-11-11 18:30:27 +0900

    use FiniteVarPoly in reduction

--- GB/Polynomial.lean
+++ GB/Polynomial.lean
@@ -5,10 +5,10 @@ open Monomial
 -- Finite Variable Polynomial
 def FiniteVarPoly (σ : Type) (R : Type) [CommRing R] := CFinsupp (Monomial σ) R
 
-noncomputable def ofFiniteVarPoly [DecidableEq σ] [CommRing R] : Coe (FiniteVarPoly σ R) (MvPolynomial σ R) where
+noncomputable instance ofFiniteVarPoly [DecidableEq σ] [CommRing R] : Coe (FiniteVarPoly σ R) (MvPolynomial σ R) where
   coe m := Finsupp.mapDomain ofCFinsupp.coe (ofCFinsupp.coe m)
 
-noncomputable def toFiniteVarPoly [DecidableEq σ] [CommRing R] : Coe (MvPolynomial σ R) (FiniteVarPoly σ R) where
+noncomputable instance toFiniteVarPoly [DecidableEq σ] [CommRing R] : Coe (MvPolynomial σ R) (FiniteVarPoly σ R) where
   coe m := toCFinsupp.coe (Finsupp.mapDomain toCFinsupp.coe m)
 
 instance [DecidableEq σ] [CommRing R] [NeZero (1:R)] :
@@ -35,3 +35,7 @@ instance FiniteVarPoly.instLinearOrder [DecidableEq σ] [DecidableEq R] [CommRin
 
 def FiniteVarPoly.toList [DecidableEq σ] [DecidableEq R] [CommRing R] [LinearOrder σ] [LinearOrder R] (s : Finset (FiniteVarPoly σ R)) : List (FiniteVarPoly σ R) :=
   Finset.sort (FiniteVarPoly.instLinearOrder.le) s
+
+instance [DecidableEq σ] [DecidableEq R] [CommRing R] : DecidableEq (FiniteVarPoly σ R) := CFinsupp.DecidableEq
+
+instance [CommRing R] : CommSemiring (FiniteVarPoly σ R) := sorry
--- GB/Reduction.lean
+++ GB/Reduction.lean
@@ -6,26 +6,25 @@ import Mathlib.Data.Finset.Basic
 import Mathlib.Algebra.MvPolynomial.Basic
 
 -- Reduction Procedure, aka multivariate divison algorithm
-def Generators (σ R: Type) [DecidableEq σ] [CommRing R] : Type := Finset (MvPolynomial σ R)
+def Generators (σ R: Type) [DecidableEq σ] [CommRing R] : Type := Finset (FiniteVarPoly σ R)
 
-instance Generators.instMembership (σ R: Type) [DecidableEq σ] [CommRing R] : Membership (MvPolynomial σ R) (Generators σ R) where
+instance Generators.instMembership (σ R: Type) [DecidableEq σ] [CommRing R] : Membership (FiniteVarPoly σ R) (Generators σ R) where
   mem := Finset.instMembership.mem
 
-def MvPolynomial.div [DecidableEq σ] [Field R] (f g : MvPolynomial σ R) (g_nonzero : g ≠ 0) : (MvPolynomial σ R) × (MvPolynomial σ R) := sorry
+def FiniteVarPoly.div [DecidableEq σ] [Field R] (f g : FiniteVarPoly σ R) (g_nonzero : g ≠ 0) : (FiniteVarPoly σ R) × (FiniteVarPoly σ R) := sorry
 
-lemma MvPolynomial.div_correct [DecidableEq σ] [ord : MonomialOrder σ] [Field R] (f g : MvPolynomial σ R) (g_nonzero : g ≠ 0):
-  let (h,r) := MvPolynomial.div f g g_nonzero;
+lemma FiniteVarPoly.div_correct [DecidableEq σ] [ord : MonomialOrder σ] [Field R] (f g : FiniteVarPoly σ R) (g_nonzero : g ≠ 0):
+  let (h,r) := FiniteVarPoly.div f g g_nonzero;
   f = g*h+r ∧
-  (r = 0 ∨ ∀m ∈ monomials r, ¬ Monomial.instDvd.dvd (leading_monomial g g_nonzero) m) := sorry
+  (r = 0 ∨ ∀m ∈ monomials (ofFiniteVarPoly.coe r), ¬ Monomial.instDvd.dvd (leading_monomial (ofFiniteVarPoly.coe g) sorry) m) := sorry
 
-def Mvpolynomial.multidiv [DecidableEq σ] [Field R] (s : MvPolynomial σ R) (F : Generators σ R) (F_nonzero : ∀ f ∈ F, f ≠ 0) :
-    (CFinsupp (MvPolynomial σ R) (MvPolynomial σ R)) × (MvPolynomial σ R) :=
+def Mvpolynomial.multidiv [DecidableEq σ] [DecidableEq R] [LinearOrder σ] [LinearOrder R] [Field R] (s : FiniteVarPoly σ R) (F : Generators σ R) (F_nonzero : ∀ f ∈ F, f ≠ 0) :
+    (CFinsupp (FiniteVarPoly σ R) (FiniteVarPoly σ R)) × (FiniteVarPoly σ R) := by
+  have FList := FiniteVarPoly.toList F
+
   sorry
 
--- this will be replaced by CFinsupp.DecidableEq as MvPolynomial is replaced by FiniteVarPoly.
-instance [DecidableEq σ] [CommRing R] : DecidableEq (MvPolynomial σ R) := sorry
-
-lemma Mvpolynomial.multidiv_correct [DecidableEq σ] [ord : MonomialOrder σ] [Field R] (f : MvPolynomial σ R) (F : Generators σ R) (F_nonzero : ∀ f ∈ F, f ≠ 0):
+lemma Mvpolynomial.multidiv_correct [DecidableEq σ] [DecidableEq R] [LinearOrder σ] [LinearOrder R] [ord : MonomialOrder σ] [Field R] (f : FiniteVarPoly σ R) (F : Generators σ R) (F_nonzero : ∀ f ∈ F, f ≠ 0):
     let (a,r) := Mvpolynomial.multidiv f F F_nonzero;
     f = r + (∑ (f ∈ F), (a f)*(f)) /\
-    (r = 0 ∨ ∀m ∈ monomials r, ∀ (inF : f ∈ F), ¬ Monomial.instDvd.dvd (leading_monomial f (F_nonzero f inF)) m) := sorry
+    (r = 0 ∨ ∀m ∈ monomials (ofFiniteVarPoly.coe r), ∀ (inF : f ∈ F), ¬ Monomial.instDvd.dvd (leading_monomial (ofFiniteVarPoly.coe f) sorry) m) := sorry
