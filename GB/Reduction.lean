7b7f037d954bc90aaf08b675d0993f1ecb058bae
Yeonwoo-Nam <9921ska@gmail.com>
2024-11-19 17:03:02 +0900

    part 1 of multidiv_correct

--- GB/Reduction.lean
+++ GB/Reduction.lean
@@ -27,9 +27,12 @@ noncomputable def MvPolynomial.div [DecidableEq σ] [Field R] (f : MvPolynomial 
   (f.divMonomial (g.toMonomial g_ismon), MvPolynomial.instSub.sub f (f.divMonomial (g.toMonomial g_ismon)))
 
 lemma MvPolynomial.div_correct [DecidableEq σ] [ord : MonomialOrder σ] [Field R] (f : MvPolynomial σ R) (g : MvPolynomial σ R) (g_ismon : is_monomial g):
-  let (h,r) := MvPolynomial.div f g g_ismon;
+  let (h,r) := f.div g g_ismon;
   f = g*h+r ∧
   (r = 0 ∨ ∀m ∈ monomials r, ¬ Monomial.instDvd.dvd (@leading_monomial σ _ _ _ ord g (is_monomial_nonzero g_ismon)) m) := sorry
+
+-- Opaque
+attribute [irreducible] MvPolynomial.div
 
 noncomputable def MvPolynomial.multidiv_help [DecidableEq σ] [DecidableEq R] [LinearOrder σ] [Field R] (s : MvPolynomial σ R) (F : List (MvPolynomial σ R)) (F_isMonomial : ∀ f ∈ F, is_monomial f): (Finsupp (MvPolynomial σ R) (MvPolynomial σ R)) × (MvPolynomial σ R) :=
   match F with
@@ -48,42 +51,36 @@ noncomputable def MvPolynomial.multidiv [DecidableEq σ] [DecidableEq R] [Linear
     (Finsupp (MvPolynomial σ R) (MvPolynomial σ R)) × (MvPolynomial σ R) :=
   MvPolynomial.multidiv_help s (F.toList) (FList_isMonomial F_isMonomial)
 
-lemma MvPolynomial.multidiv_correct [DecidableEq σ] [DecidableEq R] [LinearOrder σ] [ord : MonomialOrder σ] [Field R] (s : MvPolynomial σ R) (F : Finset (MvPolynomial σ R)) (F_isMonomial : ∀ f ∈ F, is_monomial f):
+lemma Finset.sumEQ [CommRing R] (s: Finset (MvPolynomial σ R)) (f: (MvPolynomial σ R) -> (MvPolynomial σ R)): s.sum f = (s.toList.map f).sum := by
+  unfold Finset.sum
+  rw [← Multiset.sum_toList]
+  have := Multiset.map_coe f s.toList
+  simp at this; rw [this]; clear this; simp
+
+lemma MvPolynomial.multidiv_correct [DecidableEq R] [LinearOrder σ] [ord : MonomialOrder σ] [Field R] (s : MvPolynomial σ R) (F : Finset (MvPolynomial σ R)) (F_isMonomial : ∀ f ∈ F, is_monomial f):
     -- let (a,r) := (MvPolynomial.multidiv s F F_isMonomial);
     s = (s.multidiv F F_isMonomial).snd + (∑ (f ∈ F), ((s.multidiv F F_isMonomial).fst f)*(f)) /\
-    ((s.multidiv F F_isMonomial).snd = 0 ∨ ∀m ∈ monomials (s.multidiv F F_isMonomial).snd, ∀ f (inF : f ∈ F), ¬ Monomial.instDvd.dvd (leading_monomial f (is_monomial_nonzero (F_isMonomial f inF))) m) := by sorry
-  -- unfold multidiv; simp
-  -- constructor
-  -- .
-  --   -- have F_isMonomial' := FList_isMonomial F F_isMonomial
-  --   have : ∀ l (s : FiniteVarPoly σ R) F F_isMonomial (EQ: l = toList F), s = (multidiv_help s l (by rw[EQ]; exact FList_isMonomial F F_isMonomial)).2 + (∑ (f ∈ F), ((multidiv_help s l (by rw[EQ]; exact FList_isMonomial F F_isMonomial)).1 f)*(f)) := by
-  --     clear s F F_isMonomial
-  --     intro l s F F_isMonomial EQ
-  --     have F_isMonomial' := FList_isMonomial F F_isMonomial
-  --     rw [← EQ] at F_isMonomial'
-  --     -- replace (FList_isMonomial F F_isMonomial) := F_isMonomial'
-  --     -- clear F_isMonomial
-  --     have EQ' : F = l.toFinset := by
-  --       rw [EQ]; symm; unfold toList; apply Finset.sort_toFinset
-  --     -- rw [EQ']
-  --     have G : ∀ l (s : FiniteVarPoly σ R) (l_isMonomial := l.all fun f ↦ decide (f ≠ 0)), s = (s.multidiv_help l sorry).2 + (l.map (fun f => (s.multidiv_help l sorry).1 f * f)).sum := by
-  --       intro l; induction' l with f F' IH
-  --       <;> intro s l_isMonomial
-  --       . unfold multidiv_help; simp
-  --         rw [@List.sum_nil]
-  --         have : F = ∅ := by
-  --           apply Finset.eq_empty_of_forall_not_mem
-  --           intro f
-  --           have EQ' : ¬ ∃(f : FiniteVarPoly σ R), f ∈ toList F := by
-  --             rw [← EQ]
-  --             rintro ⟨f,h1,h2⟩
-  --           by_contra G; apply EQ'
-  --           exists f; unfold toList; rw [Finset.mem_sort]; assumption
-  --         rw [this, Finset.sum_eq_fold]; simp;
-  --         clear this EQ F_isMonomial F
-  --         FiniteVarPoly is CommRing
-  --         sorry
-  --       . simp [multidiv_help]
-  --         have IH' := IH (div s f (by sorry)).2 F'
-  --         sorry
-  --   apply this; rfl; assumption
+    ((s.multidiv F F_isMonomial).snd = 0 ∨ ∀m ∈ monomials (s.multidiv F F_isMonomial).snd, ∀ f (inF : f ∈ F), ¬ Monomial.instDvd.dvd (leading_monomial f (is_monomial_nonzero (F_isMonomial f inF))) m) := by
+  unfold multidiv; constructor
+  . have : ∀ l (s : MvPolynomial σ R) F F_isMonomial (EQ: l = F.toList),
+    s = (multidiv_help s l (by rw[EQ]; exact FList_isMonomial F_isMonomial)).2 + (∑ (f ∈ F), ((multidiv_help s l (by rw[EQ]; exact FList_isMonomial F_isMonomial)).1 f)*(f)) := by
+      clear s F F_isMonomial
+      intro l s F F_isMonomial EQ
+      have F_isMonomial' := FList_isMonomial F_isMonomial
+      rw [← EQ] at F_isMonomial'
+      have EQ' : F = l.toFinset := by rw [EQ]; simp
+      have G : ∀ l (s : MvPolynomial σ R) (l_isMonomial : ∀ f ∈ l, is_monomial f), s = (s.multidiv_help l l_isMonomial).2 + (l.map (fun f => (s.multidiv_help l l_isMonomial).1 f * f)).sum := by
+        intro l; induction' l with f F' IH
+        <;> intro s l_isMonomial
+        . unfold multidiv_help; simp
+        . simp [multidiv_help]
+          have IH' := IH (div s f (by apply l_isMonomial; apply List.mem_cons_self)).2 (by intros; apply l_isMonomial; apply List.mem_cons_of_mem; assumption)
+          have div_correct := (MvPolynomial.div_correct s f (by apply l_isMonomial; apply List.mem_cons_self)).1
+          nth_rewrite 1 [div_correct]
+          nth_rewrite 1 [IH']
+          -- rw [IH']
+          sorry
+      nth_rewrite 1 [G l s F_isMonomial'];
+      rw [Finset.sumEQ]; congr!
+    apply this; rfl; assumption
+  . sorry
