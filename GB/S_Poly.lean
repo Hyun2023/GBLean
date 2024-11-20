84d8b87f05cce4276d5ce7b94530b699cf1f7588
Jaewoo Kim <jaewoo.kim@sf.snu.ac.kr>
2024-11-20 16:05:08 +0900

    spol

--- GB/S_Poly.lean
+++ GB/S_Poly.lean
@@ -13,8 +13,23 @@ noncomputable def Spol_help [DecidableEq σ] [Field R] [ord : MonomialOrder σ ]
   MvPolynomial.instSub.sub
     (MvPolynomial.monomial ((LCM (leading_monomial f f_NE) (leading_monomial g g_NE)) / (leading_monomial f f_NE)) (Inv.inv (leading_coeff f f_NE)) * f)
     (MvPolynomial.monomial ((LCM (leading_monomial f f_NE) (leading_monomial g g_NE)) / (leading_monomial g g_NE)) (Inv.inv (leading_coeff g g_NE)) * g)
--- noncomputable def Spol [DecidableEq σ] [Field R] [DecidableEq R] [ord : MonomialOrder σ ] (f g : MvPolynomial σ R) : MvPolynomial σ R :=
---   if f_NE : f = 0 then 0 else (if g_NE : g = 0 then 0 else Spol_help f g f_NE g_NE)
+noncomputable def Spol [DecidableEq σ] [Field R] [DecidableEq R] [ord : MonomialOrder σ ] (f g : MvPolynomial σ R) : MvPolynomial σ R :=
+  if f_NE : f = 0 then 0 else (if g_NE : g = 0 then 0 else Spol_help f g f_NE g_NE)
+-- gives trivial value for zero polynomials
+
+lemma Spol_help_lemma5_help [DecidableEq σ] [Field R] [ord : MonomialOrder σ ] n (c : Fin n -> R) (f : Fin n -> MvPolynomial σ R)
+  (m : Monomial σ)
+  (NE0 : ∀ (n' : Fin n), c n' ≠ 0)
+  (NE1 : ∀ (n' : Fin n), f n' ≠ 0)
+  (MDEG1 : ∀ (n' : Fin n), leading_monomial (f n') (NE1 n') = m)
+  (NE2 : (∑ (n' : Fin n), (MvPolynomial.C (c n')) * (f n')) ≠ 0)
+  (MDEG2 : (leading_monomial (∑ (n' : Fin n), (MvPolynomial.C (c n')) * (f n')) NE2) < m) :
+  ∃ (c' : Fin n -> Fin n -> R),
+  (∑ (n' : Fin n), (MvPolynomial.C (c n')) * (f n')) = (∑ (n' : Fin n), ∑ (n'' : Fin n), (MvPolynomial.C (c' n' n'')) * (Spol_help (f n') (f n'') (NE1 n') (NE1 n''))) := by
+  let d := (fun n' => leading_coeff (f n') (NE1 n'))
+  -- induction' n with np IH; simp
+  -- simp
+  sorry
 
 lemma Spol_help_lemma5 [DecidableEq σ] [Field R] [ord : MonomialOrder σ ] n (c : Fin n -> R) (f : Fin n -> MvPolynomial σ R)
   (m : Monomial σ)
@@ -24,4 +39,8 @@ lemma Spol_help_lemma5 [DecidableEq σ] [Field R] [ord : MonomialOrder σ ] n (c
   (MDEG2 : (leading_monomial (∑ (n' : Fin n), (MvPolynomial.C (c n')) * (f n')) NE2) < m) :
   ∃ (c' : Fin n -> Fin n -> R),
   (∑ (n' : Fin n), (MvPolynomial.C (c n')) * (f n')) = (∑ (n' : Fin n), ∑ (n'' : Fin n), (MvPolynomial.C (c' n' n'')) * (Spol_help (f n') (f n'') (NE1 n') (NE1 n''))) := by
+
+  let d := (fun n' => leading_coeff (f n') (NE1 n'))
+  -- induction' n with np IH; simp
+  -- simp
   sorry
